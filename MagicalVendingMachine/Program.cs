2e22af678ce60b3a5958b1a92d25d145e65e1157
Vibe Rantz <subidriver03@gmail.com>
2025-01-15 12:20:17 -0800

    Add project files.

---
+++ MagicalVendingMachine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MagicalVendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Magical Vending Machine from Solo Leveling!");
+            Console.WriteLine("This program will allow you to select magical items.");
+        }
+    }
+}
